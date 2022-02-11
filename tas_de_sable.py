8555df321b74749baabf9de8866bb25eae0d3594
edgar.baghdasaryan@ens.uvsq.fr <edgar.baghdasaryan@ens.uvsq.fr>
2022-02-11 15:25:39 +0100

    ok

---
+++ tas_de_sable.py
@@ -0,0 +1,8 @@
+##################################
+# groupe MI 3
+# Edgar BAGHDASARYAN
+# Ali HARGAS
+# Hatim ZELAMTA
+# Anis FETOUAB
+# https://github.com/uvsq22102013/projet_tas_de_sable/settings/interaction_limits
+##################################
