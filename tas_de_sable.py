f185122b4eb920f1d0a16d7e54265735ee1e4617
edgar.baghdasaryan@ens.uvsq.fr <edgar.baghdasaryan@ens.uvsq.fr>
2022-02-11 15:27:34 +0100

    Merge branch 'main' of https://github.com/uvsq22102013/projet_tas_de_sable

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 ##################################
-=======
-#################################
->>>>>>> 305251f28fea2d9a6f1907eb0c1fba81ade8353a
 # groupe MI 3
 # Edgar BAGHDASARYAN
 # Ali HARGAS
 # Hatim ZELAMTA
 # Anis FETOUAB
 # https://github.com/uvsq22102013/projet_tas_de_sable/settings/interaction_limits
-<<<<<<< HEAD
 ##################################
-=======
-#################################
->>>>>>> 305251f28fea2d9a6f1907eb0c1fba81ade8353a
